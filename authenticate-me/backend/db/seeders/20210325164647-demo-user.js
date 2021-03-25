1e507d22fc6a33e8a838ce53260f443069b0a045
Chuck Kainrath <chuckkainrath@gmail.com>
2021-03-25 12:18:15 -0500

    configured user model

---
+++ authenticate-me/backend/db/seeders/20210325164647-demo-user.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const faker = require('faker');
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [
+      { username: 'Demo-User', email: 'demo@user.io', hashedPassword: bcrypt.hashSync('password') },
+      { username: 'FakeUser1', email: faker.internet.email(), hashedPassword: bcrypt.hashSync(faker.internet.password()) },
+      { username: 'FakeUser2', email: faker.internet.email(), hashedPassword: bcrypt.hashSync(faker.internet.password()) }
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Users', {
+      username: { [Op.in]: ['Demo-User', 'FakeUser1', 'FakeUser2']}
+    }, {});
+  }
+};
