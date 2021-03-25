4424111469b8341ebbe7d4a39dee06a3cd4a06b8
Chuck Kainrath <chuckkainrath@gmail.com>
2021-03-25 12:42:30 -0500

    added auth middleware

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
+
+// For Testing Auth Middlewares
+// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+//   const user = await User.findOne({
+//     where: {
+//       username: 'Demo-User'
+//     },
+//   });
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// }));
 
 module.exports = router;
