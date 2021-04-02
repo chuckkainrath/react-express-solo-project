1251fa096c612b702b316cedda9466eb2c2a1396
Chuck Kainrath <chuckkainrath@gmailcom>
2021-04-02 08:59:40 -0500

    styled main layout of group page

--- frontend/src/components/GroupPageComponents/MessageBoardContainer/index.js
+++ frontend/src/components/GroupPageComponents/MessageBoardContainer/index.js
@@ -1,12 +1,13 @@
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import styles from './MessageBoardContainer.module.css';
 
 function MessageBoardContainer() {
   const { groupIdx } = useParams();
   const messages = useSelector(state => state.group.messageBoards[groupIdx]);
 
   return(
-    <div className='messages__container'>
+    <div className={styles.message__container}>
       <h1>Message Board</h1>
       {messages.map(message => {
         return (
