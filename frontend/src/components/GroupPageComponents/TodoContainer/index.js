094996e5531e26e179e6ae2b8e77f12c4dea5dba
Chuck Kainrath <chuckkainrath@gmail.com>
2021-04-02 08:59:40 -0500

    styled main layout of group page

--- frontend/src/components/GroupPageComponents/TodoContainer/index.js
+++ frontend/src/components/GroupPageComponents/TodoContainer/index.js
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import TodoTasks from '../TodoTasks';
+import styles from './TodoContainer.module.css';
 
 function TodoContainer() {
   const { groupIdx } = useParams();
@@ -8,7 +9,7 @@ function TodoContainer() {
   const todoTasks = useSelector(state => state.group.todoItems[groupIdx]);
 
   return (
-    <div className='todo__container'>
+    <div className={styles.todo__container}>
       <h1>Todo List</h1>
       {todoGroups.map((group, idx) => {
         return <TodoTasks key={idx} taskGroup={group.title}>{todoTasks[idx]}</TodoTasks>
